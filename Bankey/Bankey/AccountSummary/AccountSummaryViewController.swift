39d92a61690a65ae096f277b72116c17e3b7d0c6
9hus4r <perdanaagung71@gmail.com>
2022-11-20 22:10:28 +0700

    Feat: Style header view

--- Bankey/Bankey/AccountSummary/AccountSummaryViewController.swift
+++ Bankey/Bankey/AccountSummary/AccountSummaryViewController.swift
@@ -34,6 +34,7 @@ extension AccountSummaryViewController {
     
     private func setup(){
         setupTableView()
+        setupTableHeaderView()
     }
     
     private func setupTableView(){
@@ -49,6 +50,16 @@ extension AccountSummaryViewController {
             tableView.trailingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor),
             tableView.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor)
         ])
+    }
+    
+    private func setupTableHeaderView(){
+        let header = AccountSummaryHeaderView(frame: .zero)
+        
+        var size = header.systemLayoutSizeFitting(UIView.layoutFittingCompressedSize)
+        size.width = UIScreen.main.bounds.width
+        header.frame.size = size
+        
+        tableView.tableHeaderView = header
     }
 }
 
